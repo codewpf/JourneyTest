02c89d4b71db807e7dcf48fed12d9bf9e05f5d9b
Alex <ioswpf@gmail.com>
2021-11-25 10:30:43 +1300

    Add file

---
+++ Utils/Structs/JTUITestKeys.swift
@@ -0,0 +1,20 @@
+//
+//  JTUITestKeys.swift
+//  JourneyTest
+//
+//  Created by Alex on 25/11/21.
+//
+
+import Foundation
+
+struct JTUITestKeys {
+    static let keys: JTUITestKeys = JTUITestKeys()
+    
+    private init(){}
+    
+    let postNormalFirstCell: String = "List-Post-Normal-FirstCell"
+    let detailNormalFirstCell: String = "Detail-Normal-FirstCell"
+    let detailCellBodyLabel: String = "Detail-Cell-BodyLabel"
+    let detailCellNameLabel: String = "Detail-Cell-NameLabel"
+    
+}
